635544fdf8e7db05848413ad556343b90fb588f2
jenjhayden <jenjhayden@gmail.com>
2020-05-28 21:39:16 -0700

    working code

--- web_scrapper_indeed.py
+++ web_scrapper_indeed.py
@@ -1,41 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 source = requests.get('https://www.indeed.com/jobs?q=python+developer&l=').text
-soup = BeautifulSoup(source,'lmxl')
 
-for jobs in soup.find_all(class_='result'):
-    #print(jobs.precertify())
+soup = BeautifulSoup(source,'html5lib')
 
-try:
-    job_title = jobs.h2.text.strip()
-except Exception as e:
-    job_title = None
-print('Job_Title:', job_title)
+for jobs in soup.find_all (class_='result'):
 
-try:
-    company = jobs.span.text.strip()
-except Exception as e:
-    company= None
-print('Company:', company)
+    try:
+        title = jobs.h2.text.strip()
+    except Exception as e:
+        title = None
+    print('Job Title:', title)
 
-try:
-    location = jobs.find('span', class_='location').text.strip()
-except Exception as e:
-    location = None
-print('Location:', location)
+    try:
+        company = jobs.span.text.strip()
+    except Exception as e:
+        company= None
+    print('Company:', company)
 
-try:
-    summary = jobs.find('span', class_='summary').text.strip()
-except Exception as e:
-    summary = None
-print('Summary:', summary)
+    try:
+        location = jobs.find('span', class_='location').text.strip()
+    except Exception as e:
+        location = None
+    print('Location:', location)
 
-try:
-    salary = jobs.find('span',class_='no-wrap').text.strip()
-except Exception as e:
-    salary = None
-print('salary:', salary)
+    try:
+        summary = jobs.find('span', class_='summary').text.strip()
+    except Exception as e:
+        summary = None
+    print('Summary:', summary)
 
-print('------------------')
+    try:
+        salary = jobs.find('span', class_='no-wrap').text.strip()
+    except Exception as e:
+        salary = None
+    print('salary:', salary)
+
+    print('------------------')
 
