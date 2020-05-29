444d7beded3c3a30a7fe097e7efe55453d800604
jenjhayden <jenjhayden@gmail.com>
2020-05-28 20:34:38 -0700

    edit

--- web_scrapper_indeed.py
+++ web_scrapper_indeed.py
@@ -2,3 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 
 source = requests.get('https://www.indeed.com/jobs?q=python+developer&l=').text
+soup = BeautifulSoup(source,'lmxl')
+
+for jobs in soup.find_all(class_='result'):
+    #print(jobs.precertify())
+
+try:
+    job_title = jobs.h2.text.strip()
+except Exception as e:
+    job_title = None
+print('Job_Title:', job_title)
+
+try:
+    company = jobs.span.text.strip()
+except Exception as e:
+    company= None
+print('Company:', company)
+
+try:
+    location = jobs.find('span', class_='location').text.strip()
+except Exception as e:
+    location = None
+print('Location:', location)
+
+try:
+    summary = jobs.find('span', class_='summary').text.strip()
+except Exception as e:
+    summary = None
+print('Summary:', summary)
+
+try:
+    salary = jobs.find('span',class_='no-wrap').text.strip()
+except Exception as e:
+    salary = None
+print('salary:', salary)
+
+print('------------------')
+
